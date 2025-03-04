a792d67d75e9099930221debd24fba4d3864209b
ShiroCruck <107267337+ShiroCruck@users.noreply.github.com>
2025-03-04 18:05:51 +0100

    Add files via upload
    La bomba llama a arder() tras 3 segundos
    BloqueArdiendo llama a dejarDeArder() tras 2 segundos

--- WS/Bomberman/src/observer/Bomba.java
+++ WS/Bomberman/src/observer/Bomba.java
@@ -1,13 +1,22 @@
 package observer;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Bomba extends Observable
 {
-	int[] coordenadas;
+	private Timer timer = null;
 	
-	public Bomba()
+	public Bomba(int pI,int pJ)
 	{
-		coordenadas = new int[2];
+		TimerTask timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				MatrizBloques.getMB().arder(pI, pJ);
+			}		
+		};
+		timer = new Timer();
+		timer.scheduleAtFixedRate(timerTask, 0, 3000);
 	}
 }
