ef5b25c0614de6a7da41c50f207f89d23acfe819
Tupanga <128382507+Tupanga@users.noreply.github.com>
2025-03-04 18:37:32 +0100

    Pantalla para partida perdida

---
+++ WS/Bomberman/src/observable/Dead_Window.java
@@ -0,0 +1,55 @@
+package observable;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+
+public class Dead_Window extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Dead_Window frame = new Dead_Window();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Dead_Window() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new LineBorder(new Color(237, 51, 59), 20, true));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("PARTIDA ACABADA");
+		lblNewLabel.setBounds(163, 128, 154, 51);
+		contentPane.add(lblNewLabel);
+	}
+}
