e2ac713753a2b94d6d39aa0ccbe4d6d85f1f472b
plopez053 <plopez053@ikasle.ehu.eus>
2025-03-04 16:56:05 +0100

    Implementación de la clase MatrizBloques y mejoras en MainFrame para la visualización de bloques

--- WS/Bomberman/src/observable/MainFrame.java
+++ WS/Bomberman/src/observable/MainFrame.java
@@ -2,41 +2,74 @@ package observable;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import observer.Bloque;
+import observer.BloqueBlando;	
+import observer.BloqueDuro;
+import observer.BloqueVacio;
+import observer.MatrizBloques;
 
 public class MainFrame extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private MatrizBloques matrizBloques;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainFrame frame = new MainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainFrame frame = new MainFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public MainFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    /**
+     * Create the frame.
+     */
+    public MainFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        
+        matrizBloques = MatrizBloques.getMB();
+        matrizBloques.inicializarPantallaClasica();
+        
+        contentPane = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Bloque[][] matriz = matrizBloques.getMatriz();
+                for (int i = 0; i < matriz.length; i++) {
+                    for (int j = 0; j < matriz[i].length; j++) {
+                        if (matriz[i][j] instanceof BloqueDuro) {
+                            g.drawString("D", j * 20, i * 20);
+                        } else if (matriz[i][j] instanceof BloqueBlando) {
+                            g.drawString("B", j * 20, i * 20);
+                        } else if (matriz[i][j] instanceof BloqueVacio) {
+                            g.drawString("V", j * 20, i * 20);
+                        } else {
+                            g.drawString("E", j * 20, i * 20);
+                        }
+                    }
+                }
+            }
+        };
+        
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new GridLayout(11, 17));
 
-		setContentPane(contentPane);
-	}
-
+        
+        setContentPane(contentPane);
+    }
 }
--- WS/Bomberman/src/observer/MatrizBloques.java
+++ WS/Bomberman/src/observer/MatrizBloques.java
@@ -1,118 +1,92 @@
 package observer;
 import java.util.Observable;
 import java.util.Random;
-import java.util.Observer;
 
-public class MatrizBloques extends Observable
-{
-	private static MatrizBloques miMB = new MatrizBloques();
-	private Bloque[][] matriz;
-	
-	private MatrizBloques() 
-	{
-		//11x17 (11 filas y 17 columnas)
-		matriz = new Bloque[11][17];
-	}
-	
-	public static MatrizBloques getMB()
-	{
-		return miMB;
-	}
-	
-	//Genera una pantalla con bloques duros en casillas impares, y bloques duros y blandos en las demas
-	public void inicializarPantallaClasica()
-	{
-		generarBloquesDuros();
-		generarBloquesVaciosYBlandos();
-		imprimirMatriz();
-	}
-	
-	//Genera bloques duros en casillas impares
-	private void generarBloquesDuros()
-	{
-		//Se generan bloques duros en las posiciones impares: (1,1), (1,3), (1,5)...
-		for (int i=1;i<11;i+=2)
-		{
-			for (int j=1;j<17;j+=2)
-			{
-				matriz[i][j] = new BloqueDuro();
-			}
-		}
-	}
-	
-	//Genera bloques blandos y vacios en las casillas no asignadas
-	private void generarBloquesVaciosYBlandos()
-	{
-		//Si no hay una casilla, se genera aleatoriamente un bloque vacio o uno blando, menos en las coords. iniciales, donde se genera uno vacío sí o sí
-		Random r = new Random();
-		for (int i=0;i<11;i++)
-		{
-			for (int j=0;j<17;j++)
-			{
-				if(matriz[i][j] == null)
-				{
-					if((i==0 && j==0) || (i==0 && j==1) || (i==1 && j==0))
-					{
-						matriz[i][j] = new BloqueVacio();
-					}
-					else 
-					{
-						int x = r.nextInt(2);
-						
-						switch(x) 
-						{
-							case 0:
-								matriz[i][j] = new BloqueVacio();
-								break;
-								
-							case 1:
-								matriz[i][j] = new BloqueBlando();
-								break;
-						}
-					}
-				}
-			}
-		}
-	}
+public class MatrizBloques extends Observable {
+    private static MatrizBloques miMB = new MatrizBloques();
+    private Bloque[][] matriz;
 
-	//Metodo para debug: Imprime la matriz de bloques en la consola
-	private void imprimirMatriz() 
-	{
-		//E:Error V:Vacio B:Blando D:Duro
-		for(int i=0;i<11;i++)
-		{
-			for(int j=0;j<17;j++)
-			{
-				if(matriz[i][j] == null)
-				{
-					System.out.print("E ");
-				}
-				else if (matriz[i][j] instanceof BloqueVacio)
-				{
-					System.out.print("V ");
-				}
-				else if (matriz[i][j] instanceof BloqueBlando)
-				{
-					System.out.print("B ");
-				}
-				else
-				{
-					System.out.print("D ");
-				}
-			}
-			System.out.println();
-		}
-	}
+    private MatrizBloques() {
+        // 11x17 (11 filas y 17 columnas)
+        matriz = new Bloque[11][17];
+    }
 
-	//Indica si en la fila pI y la columna pJ hay un bloque ardiendo
-	public boolean estaArdiendo(int pI, int pJ)
-	{
-		return (matriz[pI][pJ] instanceof BloqueArdiendo);
-	}
+    public static MatrizBloques getMB() {
+        return miMB;
+    }
 
-	//Indica si en la fila pI y la columna pJ hay un bloque que impide el paso
-	public boolean hayBloque(int pI, int pJ)
-	{
-		return (matriz[pI][pJ] instanceof BloqueBlando || matriz[pI][pJ] instanceof BloqueDuro);
-	}
+    public Bloque[][] getMatriz() {
+        return matriz;
+    }
+
+    // Genera una pantalla con bloques duros en casillas impares, y bloques duros y blandos en las demas
+    public void inicializarPantallaClasica() {
+        generarBloquesDuros();
+        generarBloquesVaciosYBlandos();
+        imprimirMatriz();
+    }
+
+    // Genera bloques duros en casillas impares
+    private void generarBloquesDuros() {
+        // Se generan bloques duros en las posiciones impares: (1,1), (1,3), (1,5)...
+        for (int i = 1; i < 11; i += 2) {
+            for (int j = 1; j < 17; j += 2) {
+                matriz[i][j] = new BloqueDuro();
+            }
+        }
+    }
+
+    // Genera bloques blandos y vacios en las casillas no asignadas
+    private void generarBloquesVaciosYBlandos() {
+        // Si no hay una casilla, se genera aleatoriamente un bloque vacio o uno blando, menos en las coords. iniciales, donde se genera uno vacío sí o sí
+        Random r = new Random();
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 17; j++) {
+                if (matriz[i][j] == null) {
+                    if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
+                        matriz[i][j] = new BloqueVacio();
+                    } else {
+                        int x = r.nextInt(2);
+                        switch (x) {
+                            case 0:
+                                matriz[i][j] = new BloqueVacio();
+                                break;
+                            case 1:
+                                matriz[i][j] = new BloqueBlando();
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Metodo para debug: Imprime la matriz de bloques en la consola
+    private void imprimirMatriz() {
+        // E:Error V:Vacio B:Blando D:Duro
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 17; j++) {
+                if (matriz[i][j] == null) {
+                    System.out.print("E ");
+                } else if (matriz[i][j] instanceof BloqueVacio) {
+                    System.out.print("V ");
+                } else if (matriz[i][j] instanceof BloqueBlando) {
+                    System.out.print("B ");
+                } else {
+                    System.out.print("D ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Indica si en la fila pI y la columna pJ hay un bloque ardiendo
+    public boolean estaArdiendo(int pI, int pJ) {
+        return (matriz[pI][pJ] instanceof BloqueArdiendo);
+    }
+
+    // Indica si en la fila pI y la columna pJ hay un bloque que impide el paso
+    public boolean hayBloque(int pI, int pJ) {
+        return (matriz[pI][pJ] instanceof BloqueBlando || matriz[pI][pJ] instanceof BloqueDuro);
+    }
 }
