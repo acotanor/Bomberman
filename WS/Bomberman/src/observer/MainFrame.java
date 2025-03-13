153ed2464c8102458e10fe8b01bd7ab959c461b3
ShiroCruck <107267337+ShiroCruck@users.noreply.github.com>
2025-03-13 20:40:12 +0100

    Add files via upload

--- WS/Bomberman/src/observer/MainFrame.java
+++ WS/Bomberman/src/observer/MainFrame.java
@@ -1,9 +1,7 @@
 package observer;
 
-import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,8 +12,6 @@ import java.awt.Image;
 import java.awt.event.*;
 import java.util.Observer;
 import java.util.Observable;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame implements Observer {
 
@@ -50,13 +46,15 @@ public class MainFrame extends JFrame implements Observer {
     	observable.MatrizBloques.getMB().addObserver(this);
     	observable.Bomberman.getBom().addObserver(this);
     	
-    	inicializarListener();
+    	controlador.inicializarListener();
     	
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 700, 450);
         
         contentPane = new JPanel(){
-            private Image imagen = new ImageIcon(MainFrame.class.getResource("/Imgs/stageBack1.png")).getImage(); // Carga la imagen
+            
+			private static final long serialVersionUID = 1L;
+			private Image imagen = new ImageIcon(MainFrame.class.getResource("/Imgs/stageBack1.png")).getImage(); // Carga la imagen
 
             @Override
             protected void paintComponent(Graphics g) {
@@ -81,31 +79,8 @@ public class MainFrame extends JFrame implements Observer {
         
         controlador.iniciar();
     }
-
-    private void inicializarListener()
-    {
-    	addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                int keyCode = e.getKeyCode();
-                
-                // Detectar las teclas de flechas y pasar al controlador
-                if (keyCode == KeyEvent.VK_UP) {
-                    controlador.moverArriba();
-                } else if (keyCode == KeyEvent.VK_DOWN) {
-                    controlador.moverAbajo();
-                } else if (keyCode == KeyEvent.VK_LEFT) {
-                    controlador.moverIzquierda();
-                } else if (keyCode == KeyEvent.VK_RIGHT) {
-                    controlador.moverDerecha();
-                }
-                else if (keyCode == KeyEvent.VK_B) {
-                    controlador.ponerBomba();
-                }
-            }
-        });
-    }
-    
+	
+	//Se llama a un metodo que actualiza la vista en funcion del argumento recibido
     @Override
 	public void update(Observable o, Object arg) 
 	{
@@ -113,145 +88,166 @@ public class MainFrame extends JFrame implements Observer {
     	
 		if(msg.startsWith("Bloque"))
 		{
-			String[] split = msg.split(",");
-			int i = Integer.valueOf(split[1]);
-			int j = Integer.valueOf(split[2]);
-			
-			if(msg.startsWith("BloqueDuro"))
-			{
-            	labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/hard5.png")));
-			}
-			else if(msg.startsWith("BloqueBlando"))
-			{
-            	labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/soft1.png")));
-			}
-			else if(msg.startsWith("BloqueVacio"))
-			{
-				labels[i][j].setIcon(new ImageIcon());
-			}
-			else if(msg.startsWith("BloqueArdiendo"))
-			{
-				String anim = "1";
-				
-				if(msg.startsWith("BloqueArdiendoA"))
-				{
-					anim = split[3];
-				}
-				
-				labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/kaBomb" + anim + ".png")));
-			}
+			actualizarBloque(msg);
 		}
 		else if(msg.startsWith("Bomber"))
 		{
-			String[] split = msg.split(",");
-			int i = Integer.valueOf(split[1]);
-			int j = Integer.valueOf(split[2]);
-			String direccion = split[3];
-			boolean hayBomba = Boolean.valueOf(split[4]);
-			
-			if(ultimaDir.equals(direccion))
-			{
-				anim++;
-				if((direccion.equals("Abajo") && anim>4) || anim>5)
-				{
-					anim = 1;
-				}
-			}
-			else 
-			{
-				anim = 1;
-				ultimaDir = direccion;
-			}
-			
-			String icono ="";
-			if(hayBomba) 
-			{
-				icono = "/Imgs/bomb1.png";
-			}
-			
-			if(direccion.equals("Izquierda"))
-			{
-				labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whiteleft" + String.valueOf(anim) + ".png")));
-				labels[i][j+1].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
-			}
-			else if(direccion.equals("Arriba"))
-			{
-				labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whiteup" + String.valueOf(anim) + ".png")));
-				labels[i+1][j].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
-			}
-			else if(direccion.equals("Abajo"))
-			{
-				labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whitedown" + String.valueOf(anim) + ".png")));
-				labels[i-1][j].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
-			}
-			else
-			{
-				if(direccion.equals("Derecha"))
-				{
-					labels[i][j-1].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
-				}
-				labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whiteright" + String.valueOf(anim) + ".png")));
-			}
+			actualizarBomber(msg);
 		}
 		else if(msg.startsWith("Bomba"))
 		{
-			String[] split = msg.split(",");
-			int i = Integer.valueOf(split[1]);
-			int j = Integer.valueOf(split[2]);
-			
-			labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whitewithbomb1.png")));
+			actualizarBomba(msg);
 		}
 		else if(msg.startsWith("Dead") && !finished)
 		{
-			finished = true;
-			String[] split = msg.split(",");
-			int i = Integer.valueOf(split[1]);
-			int j = Integer.valueOf(split[2]);
-			
-			labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/onFire1.png")));
-			
-			Dead_Window jf = new Dead_Window();
-			jf.setVisible(true);
+			actualizarMuerte(msg);
 		}
 	}
-
-    private class Controlador implements ActionListener 
+    
+    private void actualizarBloque(String msg)
     {
-		@Override
-		public void actionPerformed(ActionEvent e) 
+    	String[] split = msg.split(",");
+		int i = Integer.valueOf(split[1]);
+		int j = Integer.valueOf(split[2]);
+		
+		if(msg.startsWith("BloqueDuro"))
 		{
-				
+        	labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/hard5.png")));
 		}
+		else if(msg.startsWith("BloqueBlando"))
+		{
+        	labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/soft1.png")));
+		}
+		else if(msg.startsWith("BloqueVacio"))
+		{
+			labels[i][j].setIcon(new ImageIcon());
+		}
+		else if(msg.startsWith("BloqueArdiendo"))
+		{
+			String animacion = "1";
+			
+			if(msg.startsWith("BloqueArdiendoA"))
+			{
+				animacion = split[3];
+			}
+			
+			labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/kaBomb" + animacion + ".png")));
+		}
+    }
+    
+    private void actualizarBomber(String msg)
+    {
+    	String[] split = msg.split(",");
+		int i = Integer.valueOf(split[1]);
+		int j = Integer.valueOf(split[2]);
+		String direccion = split[3];
+		boolean hayBomba = Boolean.valueOf(split[4]);
+		
+		if(ultimaDir.equals(direccion))
+		{
+			anim++;
+			if((direccion.equals("Abajo") && anim>4) || anim>5)
+			{
+				anim = 1;
+			}
+		}
+		else 
+		{
+			anim = 1;
+			ultimaDir = direccion;
+		}
+		
+		String icono ="";
+		if(hayBomba) 
+		{
+			icono = "/Imgs/bomb1.png";
+		}
+		
+		if(direccion.equals("Izquierda"))
+		{
+			labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whiteleft" + String.valueOf(anim) + ".png")));
+			labels[i][j+1].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
+		}
+		else if(direccion.equals("Arriba"))
+		{
+			labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whiteup" + String.valueOf(anim) + ".png")));
+			labels[i+1][j].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
+		}
+		else if(direccion.equals("Abajo"))
+		{
+			labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whitedown" + String.valueOf(anim) + ".png")));
+			labels[i-1][j].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
+		}
+		else
+		{
+			if(direccion.equals("Derecha"))
+			{
+				labels[i][j-1].setIcon(new ImageIcon(MainFrame.class.getResource(icono)));
+			}
+			labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whiteright" + String.valueOf(anim) + ".png")));
+		}
+    }
+    
+    private void actualizarBomba(String msg)
+    {
+    	String[] split = msg.split(",");
+		int i = Integer.valueOf(split[1]);
+		int j = Integer.valueOf(split[2]);
+		
+		labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/whitewithbomb1.png")));
+    }
+    
+    private void actualizarMuerte(String msg)
+    {
+    	finished = true;
+		String[] split = msg.split(",");
+		int i = Integer.valueOf(split[1]);
+		int j = Integer.valueOf(split[2]);
+		
+		labels[i][j].setIcon(new ImageIcon(MainFrame.class.getResource("/Imgs/onFire1.png")));
+		
+		Dead_Window jf = new Dead_Window();
+		jf.setVisible(true);
+    }
+    
+    private class Controlador 
+    {
+		public void inicializarListener()
+	    {
+	    	addKeyListener(new KeyAdapter() {
+	            @Override
+	            public void keyPressed(KeyEvent e) {
+	            	
+	                int keyCode = e.getKeyCode();
+	                
+	                if (keyCode == KeyEvent.VK_UP) 
+	                {
+	                	observable.Bomberman.getBom().moverArriba();
+	                } 
+	                else if (keyCode == KeyEvent.VK_DOWN) 
+	                {
+	                	observable.Bomberman.getBom().moverAbajo();
+	                } 
+	                else if (keyCode == KeyEvent.VK_LEFT) 
+	                {
+	                	observable.Bomberman.getBom().moverIzquierda();
+	                } 
+	                else if (keyCode == KeyEvent.VK_RIGHT) 
+	                {
+	                	observable.Bomberman.getBom().moverDerecha();
+	                }
+	                else if (keyCode == KeyEvent.VK_B) 
+	                {
+	                	observable.Bomberman.getBom().soltarBomba();
+	                }
+	            }
+	        });
+	    }
 		
 		public void iniciar()
 		{
 			observable.MatrizBloques.getMB().inicializarPantallaClasica();
 			observable.Bomberman.getBom().notificarPosicion("Inicio",false);
-		}
-		
-		public void moverIzquierda()
-		{
-			observable.Bomberman.getBom().moverIzquierda();
-		}
-		
-		public void moverDerecha()
-		{
-			observable.Bomberman.getBom().moverDerecha();
-		}
-		
-		public void moverArriba()
-		{
-			observable.Bomberman.getBom().moverArriba();
-		}
-		
-		public void moverAbajo()
-		{
-			observable.Bomberman.getBom().moverAbajo();
-		}
-		
-		public void ponerBomba()
-		{
-			observable.Bomberman.getBom().soltarBomba();
 		}
 	}
 }
