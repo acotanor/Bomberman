6b430030eaf3d4b6e2a8c190f6be7596f0a0b8c2
ShiroCruck <107267337+ShiroCruck@users.noreply.github.com>
2025-04-08 18:52:12 +0200

    Add files via upload

--- WS/Bomberman/src/observable/Facade.java
+++ WS/Bomberman/src/observable/Facade.java
@@ -23,8 +23,7 @@ public class Facade
 	private void generarEnemigos()
 	{
 		int i = 0;
-		int max = MatrizBloques.getMB().cuantosBloquesVacios()/6;
-		System.out.println(String.valueOf(max));
+		int max = MatrizBloques.getMB().cuantosBloquesVacios()/12;
 		Random r = new Random();
 		
 		while(i<max)
@@ -32,7 +31,7 @@ public class Facade
 			int fila = r.nextInt(11);
 			int columna = r.nextInt(17);
 			
-			if(!MatrizBloques.getMB().hayBloque(fila, columna) && !ListaEnemigos.getLE().hayEnemigo(fila,columna))
+			if(!MatrizBloques.getMB().hayBloque(fila, columna) && !ListaEnemigos.getLE().hayEnemigo(fila,columna) && fila+columna>1)
 			{
 				ListaEnemigos.getLE().generarEnemigo(fila, columna);
 				i++;
